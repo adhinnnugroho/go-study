976f58e568f4f67834585f8b5785e32f82a71341
adhinnnugroho <adhinnnugroho@gmail.com>
2024-11-12 09:36:20 +0700

    study map type data

---
+++ map.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//
+	// var person map[string]string = map[string]string{}
+	// person["name"] = "John Doe"
+	// person["age"] = "30"
+
+	person := map[string]string{
+		"name": "John Doe",
+		"age":  "30",
+	}
+
+	fmt.Println(person["name"])
+	fmt.Println(person["age"])
+	fmt.Println(person)
+
+	book := make(map[string]string)
+	book["title"] = "The Go Programming Language"
+	book["author"] = "Rob Pike"
+	book["ups"] = "salah"
+
+	fmt.Println(book)
+	delete(book, "ups")
+	fmt.Println(book)
+}
